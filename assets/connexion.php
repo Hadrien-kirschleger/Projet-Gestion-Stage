a45be2a5ca4708729c57fe5d3e1e84e1dcf4f115
Hadrien Kirschleger <81999074+Hadrien-kirschleger@users.noreply.github.com>
2022-04-05 09:23:05 +0200

    Update connexion.php

--- assets/connexion.php
+++ assets/connexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$cnx = new PDO('mysql:host=localhost;dbname=kartes1', "kartes", "magicMagik123");
+$cnx = new PDO('mysql:host=localhost;dbname=exemple', "login", "password");
 $cnx -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $cnx -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
